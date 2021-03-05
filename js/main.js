d0ab5443bf02de318bf577aba5ed29b002b8f5ae
Axel JARNIGON <axel.jarnigon@gmail.com>
2021-03-05 10:34:15 +0100

    commit after big loop problem resolved

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ function getRandomWord() {
 
 //Split word into array
 function splitWord() {
-    let splitWordArray = wordChoice.split("");
+    let splitWordArray = Array.from(wordChoice);
     return splitWordArray;
 };
 
@@ -29,14 +29,15 @@ function hideTheWord(){
 };
 
 //Get the letter guessed by user and make sure it's only 1 letter long and not a number
-function getUserGuess() {
-    let userGuess = prompt("Veuillez rentrer une lettre que selon vous se trouve dans le mot à deviner !").toLowerCase();
+// function getUserGuess() {
+//     let userGuess = prompt(`Veuillez rentrer une lettre qui selon vous se trouve dans le mot à deviner !
+//     \nIl vous reste ${userScore} tentatives.\n${hiddenWord}`).toLowerCase();
 
-    while(userGuess.length !== 1 || isNaN(userGuess) == false) {
-        userGuess = prompt("Attention à ne rentrer qu'une seule lettre !\nSinon c'est de la triche...").toLowerCase();
-    };
-    return userGuess;
-};
+//     while(userGuess.length !== 1 || isNaN(userGuess) == false) {
+//         userGuess = prompt("Attention à ne rentrer qu'une seule lettre !\nSinon c'est de la triche...").toLowerCase();
+//     };
+//     return userGuess;
+// };
 
 // function checkLetterGuess(){
 //     if (splitWordArray.includes(userGuess)) {
@@ -53,6 +54,31 @@ alert("Bienvenue, nous allons maintenant jouer au jeu du Pendu !");
 getRandomWord();
 splitWord();
 hideTheWord();
-alert(`le mot caché est ${hiddenWord}`)
-let userGuess = getUserGuess();
+do {
+  var userGuess = prompt(`Veuillez rentrer une lettre qui selon vous se trouve dans le mot à deviner !
+    \nIl vous reste ${userScore} tentatives.\n${hiddenWord}`).toLowerCase();
+  while (userGuess.length !== 1 || isNaN(userGuess) === false) {
+    userGuess = prompt("Attention à ne rentrer qu'une seule lettre !\nSinon c'est de la triche...").toLowerCase();
+  };
+  if (splitWordArray.includes(userGuess)) {
+    for (var i = 0; i < splitWordArray.length; i ++) {
+      if (userGuess === splitWordArray[i]){
+        hiddenWord[i] = splitWordArray[i]; 
+      }
+    }  
+  }
+  else {
+    userScore -= 1;
+  }
+} while (userScore > 0);
+
+
+
+
+
+// getRandomWord();
+// splitWord();
+// hideTheWord();
+//alert(`le mot caché est ${hiddenWord}`)
+//let userGuess = getUserGuess();
 // checkLetterGuess();
