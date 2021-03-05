29bb8b9139398ac37d0cebc1d8e605fd7c4618d4
Axel JARNIGON <axel.jarnigon@gmail.com>
2021-03-05 16:11:51 +0100

    menu included, game is working fine but doesn't reset on second try

--- js/main.js
+++ js/main.js
@@ -6,7 +6,8 @@ let splitWordArray = splitWord();
 let hiddenWord = hideTheWord();
 let letterFound = 0;
 let alreadyGuessed = [];
-
+let userGuess = [];
+let menuInput = "";
 // FUNCTIONS
 
 //Chose the random word in the word list
@@ -30,10 +31,69 @@ function hideTheWord(){
     return hiddenWord;
 };
 
-function arrayGuessed(){
-    alreadyGuessed += userGuess + " / ";
-    return alreadyGuessed;
-}
+function game(){
+    do {
+        var userGuess = prompt(`Veuillez rentrer une lettre qui selon vous se trouve dans le mot à deviner !
+        \nIl vous reste ${userScore} tentatives.\nLes lettres déjà testées sont : ${alreadyGuessed}\n${hiddenWord}`).toLowerCase();
+        while (userGuess.length !== 1 || isNaN(userGuess) === false) {
+            userGuess = prompt("Attention à ne rentrer qu'une seule lettre !\nSinon c'est de la triche...\nLes lettres déjà testées sont : ${alreadyGuessed}").toLowerCase();
+        };
+        if (splitWordArray.includes(userGuess)) {
+            for (var i = 0; i < splitWordArray.length; i ++) {
+                if (userGuess === splitWordArray[i]){
+                    hiddenWord[i] = splitWordArray[i]; 
+                    //To avoid cheating. Gives +1 only if it's a new guess.
+                    if (alreadyGuessed.includes(userGuess) === false) {
+                        letterFound +=1;
+                    }
+                }
+            }  
+        }
+        else {
+            userScore -= 1;
+        }
+        //To avoid having alreadyGuessed field, full of spam
+        if (alreadyGuessed.includes(userGuess) === false){
+            alreadyGuessed += userGuess + " / ";
+        }
+        if (letterFound === splitWordArray.length){
+            alert(`Félicitation tu as trouvé le mot qui était : ${wordChoice}`);
+            menu();
+        }
+        else if (userScore === 0) {
+            alert(`Désolé, tu as fini pendu...\nLe mot était : ${wordChoice}`);
+            menu();
+        }
+    } while (userScore > 0);
+};
+
+function menu(){
+    let menuChoice = ["j", "r", "q"];
+    var menuInput = prompt(`Bienvenue dans le menu. Veuillez entrer la lettre correspondant à votre choix\nJ = Jouer\nR = Afficher les règles\nQ = Quitter`).toLowerCase();
+    do {
+        if (menuInput === "j") {
+            game();
+        }
+        else if (menuInput === "r") {
+            alert(`Règles du jeu`);
+            menu();
+        }
+        else if (menuInput === "q"){
+            break;
+        }
+        else {
+            menu();
+        }
+    } while (menuChoice.includes(menuInput) === false)
+};
+
+// //To show the letters that user has already guessed
+// function arrayGuessed(){
+//     alreadyGuessed += userGuess + " / ";
+//     return alreadyGuessed;
+// };
+
+
 
 //Get the letter guessed by user and make sure it's only 1 letter long and not a number
 // function getUserGuess() {
@@ -55,47 +115,6 @@ function arrayGuessed(){
 //     }
 // }
 
-// GAME CODE
-
+// GAME START
 alert("Bienvenue, nous allons maintenant jouer au jeu du Pendu !");
-getRandomWord();
-splitWord();
-hideTheWord();
-do {
-  var userGuess = prompt(`Veuillez rentrer une lettre qui selon vous se trouve dans le mot à deviner !
-    \nIl vous reste ${userScore} tentatives.\nLes lettres déjà testées sont : ${alreadyGuessed}\n${hiddenWord}`).toLowerCase();
-  while (userGuess.length !== 1 || isNaN(userGuess) === false) {
-    userGuess = prompt("Attention à ne rentrer qu'une seule lettre !\nSinon c'est de la triche...\nLes lettres déjà testées sont : ${alreadyGuessed}").toLowerCase();
-  };
-  if (splitWordArray.includes(userGuess)) {
-    for (var i = 0; i < splitWordArray.length; i ++) {
-      if (userGuess === splitWordArray[i]){
-        hiddenWord[i] = splitWordArray[i]; 
-        letterFound +=1;
-      }
-    }  
-  }
-  else {
-    userScore -= 1;
-  }
-  arrayGuessed();
-  if (letterFound === splitWordArray.length){
-      alert(`Félicitation tu as trouvé le mot qui était : ${wordChoice}`);
-      break;
-  }
-  else if (userScore === 0) {
-      alert(`Désolé, tu as fini pendu...\nLe mot était : ${wordChoice}`);
-      break;
-  }
-} while (userScore > 0);
-
-
-
-
-
-// getRandomWord();
-// splitWord();
-// hideTheWord();
-//alert(`le mot caché est ${hiddenWord}`)
-//let userGuess = getUserGuess();
-// checkLetterGuess();
+menu();
